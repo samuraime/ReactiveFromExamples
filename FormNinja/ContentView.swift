6e0957a08f61b42bc91439651481889825ab3a25
Minhui Zhao <samurai7@foxmail.com>
2021-12-19 18:33:01 +0800

    Initial Commit

---
+++ FormNinja/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FormNinja
+//
+//  Created by Minhui Zhao on 2021/12/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FormNinja/FormNinjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormNinjaApp.swift
+//  FormNinja
+//
+//  Created by Minhui Zhao on 2021/12/19.
+//
+
+import SwiftUI
+
+@main
+struct FormNinjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
